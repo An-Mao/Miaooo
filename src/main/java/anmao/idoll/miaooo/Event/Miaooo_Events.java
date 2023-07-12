a0395064fff4df5223c983a6a43b9dced1276275
An-Mao <lxxl@live.cn>
2023-07-12 19:48:49 +0800

    update

--- src/main/java/anmao/idoll/miaooo/Event/Miaooo_Events.java
+++ src/main/java/anmao/idoll/miaooo/Event/Miaooo_Events.java
@@ -1,9 +1,13 @@
 package anmao.idoll.miaooo.Event;
 
 import anmao.idoll.miaooo.Miaooo;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -45,6 +49,34 @@ public class Miaooo_Events {
                     mob.setDropChance(EquipmentSlot.LEGS, 0.0f);
                     mob.setDropChance(EquipmentSlot.MAINHAND, 0.0f);
                     mob.setDropChance(EquipmentSlot.OFFHAND, 0.0f);
+                }
+            }
+        }
+        @SubscribeEvent
+        public static void onAttack(LivingAttackEvent event){
+            if (event.getEntity() instanceof Mob mob){
+                //Entity source = event.getSource().getDirectEntity();
+                Entity source = event.getSource().getEntity();
+                if (source != null){
+                    float AAR = 3.0F;
+                    ItemStack item = source.getHandSlots().iterator().next();
+                    /*
+                    if (source instanceof LivingEntity livingEntity){
+                        if(livingEntity.getMainHandItem().getAttributeModifiers(EquipmentSlot.MAINHAND).get(Attributes.ATTACK_SPEED).isEmpty()){
+                            System.out.println("Ture");
+                            AAR = 16.0F;
+                        }
+                    }
+                     */
+                    if (item.getAttributeModifiers(EquipmentSlot.MAINHAND).get(Attributes.ATTACK_SPEED).isEmpty()){
+                        if (item.getAttributeModifiers(EquipmentSlot.MAINHAND).get(Attributes.ATTACK_DAMAGE).isEmpty()){
+                            AAR = 16.0F;
+                        }
+                    }
+                    if (Math.abs(source.getX() - mob.getX()) >AAR || Math.abs( source.getY() -mob.getY()) >AAR || Math.abs( source.getZ() -mob.getZ()) >AAR){
+                        //----------------------------------
+                        event.setCanceled(true);
+                    }
                 }
             }
         }
