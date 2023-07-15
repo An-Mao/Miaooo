ed7099c8dfcfc33d9077c60b539066617d379cfc
An-Mao <lxxl@live.cn>
2023-07-15 10:26:53 +0800

    update

--- src/main/java/anmao/idoll/miaooo/Event/Miaooo_Events.java
+++ src/main/java/anmao/idoll/miaooo/Event/Miaooo_Events.java
@@ -1,20 +1,19 @@
 package anmao.idoll.miaooo.Event;
 
 import anmao.idoll.miaooo.Miaooo;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 public class Miaooo_Events {
     @Mod.EventBusSubscriber(modid = Miaooo.MOD_ID)
     public static class ForgeEvents{
-        //
         /*
         @SubscribeEvent
         public static void onSpawned(LivingSpawnEvent.AllowDespawn event){
@@ -33,13 +32,8 @@ public class Miaooo_Events {
 
         }
         */
-        // private static float tmpi;
         @SubscribeEvent
         public static void onJoinLevel(EntityJoinLevelEvent event){
-            /*
-            tmpi++;
-            System.out.println( "["+tmpi +"]Mob spawn " + event.getEntity().getName());
-             */
             if (event.getEntity() instanceof Mob mob){
                 if(!mob.getTags().contains("MiaoooDropChance")) {
                     mob.addTag("MiaoooDropChance");
@@ -53,28 +47,41 @@ public class Miaooo_Events {
             }
         }
         @SubscribeEvent
-        public static void onAttack(LivingAttackEvent event){
-            if (event.getEntity() instanceof Mob mob){
-                //Entity source = event.getSource().getDirectEntity();
+        public static void onHurt(LivingHurtEvent event){
+            // 限制伤害值
+            if (event.getEntity() instanceof Mob mob) {
+                float damage = 0.2f;
+                if (event.getSource() == DamageSource.OUT_OF_WORLD) {
+                    damage = 0.5f;
+                }
+                damage = mob.getMaxHealth() * damage;
+                if (event.getAmount() > damage) {
+                    event.setAmount(damage);
+                    mob.addTag("MiaoooDamageSafe");
+                }
+            }
+        }
+        @SubscribeEvent
+        public static void onAttack(LivingAttackEvent event) {
+            if (event.getEntity().getTags().contains("MiaoooDamageSafe")){
+                int _tick = event.getEntity().tickCount - event.getEntity().getLastHurtByMobTimestamp();
+                if ( _tick > 0 && _tick < 20){
+                    event.setCanceled(true);
+                    return;
+                }
+                event.getEntity().removeTag("MiaoooDamageSafe");
+            }
+            if (event.getEntity() instanceof Mob mob) {
                 Entity source = event.getSource().getEntity();
-                if (source != null){
-                    float AAR = 3.0F;
-                    ItemStack item = source.getHandSlots().iterator().next();
-                    /*
-                    if (source instanceof LivingEntity livingEntity){
-                        if(livingEntity.getMainHandItem().getAttributeModifiers(EquipmentSlot.MAINHAND).get(Attributes.ATTACK_SPEED).isEmpty()){
-                            System.out.println("Ture");
-                            AAR = 16.0F;
-                        }
+                if (source != null) {
+                    float AAR = 9.0F;
+                    if (source != event.getSource().getDirectEntity()) {
+                        AAR = 256.0F;
                     }
-                     */
-                    if (item.getAttributeModifiers(EquipmentSlot.MAINHAND).get(Attributes.ATTACK_SPEED).isEmpty()){
-                        if (item.getAttributeModifiers(EquipmentSlot.MAINHAND).get(Attributes.ATTACK_DAMAGE).isEmpty()){
-                            AAR = 16.0F;
-                        }
-                    }
-                    if (Math.abs(source.getX() - mob.getX()) >AAR || Math.abs( source.getY() -mob.getY()) >AAR || Math.abs( source.getZ() -mob.getZ()) >AAR){
-                        //----------------------------------
+                    double x = source.getX() - mob.getX();
+                    double y = source.getY() - mob.getY();
+                    double z = source.getZ() - mob.getZ();
+                    if (x * x + y * y + z * z > AAR) {
                         event.setCanceled(true);
                     }
                 }
